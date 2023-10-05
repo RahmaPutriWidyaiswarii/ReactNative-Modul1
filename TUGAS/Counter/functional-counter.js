9df4351ae1f6c7c458ad01ab222805eb6fe1671b
RahmaPutriWidyaiswarii <rahma.putri.21@student.is.ittelkom-sby.ac.id>
2023-10-06 02:47:46 +0700

    update

--- TUGAS/Counter/functional-counter.js
+++ TUGAS/Counter/functional-counter.js
@@ -1,26 +1,38 @@
 // Functional Component
 const MyCounter = () => {
   // deklarasi nilai awal count "0"
-  const [count, setCount] = React.useState(0);
+    const [count, setCount] = React.useState(0);
 
-   // jumlah count +1
-  const incrementCount = () => {
-      setCount(countValue => countValue + 1)
-   }
+  // jumlah count +1
+    const incrementCount = () => {
+    setCount((countValue) => countValue + 1);
+    };
 
-  const decrementCount = () => {
-      setCount(countValue => countValue - 1)
-  }
+    const decrementCount = () => {
+    setCount((countValue) => countValue - 1);
+    };
 
-  return (
-      <>
-      <h1>{count}</h1> {/* Menampilkan jumlau count diatas tombol */}
-      <button onClick={decrementCount} style={{ margin: "0px" }}>-1</button> {/* Tombol -1, diambil dari decrementCount */}
-      <button onClick={incrementCount} style={{ margin: "0px" }}>+1</button> {/* Tombol +1, diambil dari incrementCount */}
-      </>
-  )
-}
-// hasil dipanggil
+    return (
+    <>
+      <h1>{count}</h1> {/* Menampilkan jumlah count diatas tombol */}
+        <button
+        onClick={decrementCount}
+        style={{ margin: "0px" }}
+        >
+        -1
+        </button>{" "}
+      {/* Tombol -1,  dari decrementCount */}
+        <button
+        onClick={incrementCount}
+        style={{ margin: "0px" }}
+        >
+        +1
+        </button>{" "}
+      {/* Tombol +1, dari incrementCount */}
+    </>
+    );
+};
+// code program untuk dipanggil
 const myElement = <MyCounter />;
 const myApp = myElement;
-ReactDOM.createRoot(document.getElementById('functional-counter')).render(myApp);
+ReactDOM.createRoot(document.getElementById("functional-counter")).render(myApp);
